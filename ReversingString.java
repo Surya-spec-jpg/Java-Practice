78ab9aca502d8f6701064b31432eb7d55acb7eef
Surya <sxrya007@gmail.com>
2025-05-09 12:59:57 +0530

    ReversingString.java

--- ReversingString.java
+++ ReversingString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Reversing_String {
+public class ReversingString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the String :");
